a152411035747e8031cd833d9726181709199f4b
Artur Rodrigues <arturrrocha494@gmail.com>
2025-02-25 16:52:45 -0300

    inicializando javafx

---
+++ src/controller/Controlador.java
@@ -0,0 +1,18 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class Controlador extends Declarador implements Initializable {
+
+  public Controlador(String arquivo) throws Exception {
+    super(arquivo);
+  }
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+
+  }
+
+}
